a90ddd960d943f38a9bf8aad7f40a6d6987249fc
irfan <iffi.tk@gmail.com>
2024-01-29 18:40:11 +0500

    upget

---
+++ lib/screens/SplashScreenPage.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:daudy_app/screens/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({super.key});
+
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Timer for the splash screen duration
+    Timer(const Duration(seconds: 4), () {
+      // Navigate to the home page after 4 seconds
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          height: 150,
+          // You can set other properties like width, alignment, etc.
+        ),
+      ),
+    );
+  }
+}
+
